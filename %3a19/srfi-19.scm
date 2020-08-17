fd9ae8cb0853472d54815b200e5b9805cc45270f
Göran Weinholt <goran@weinholt.se>
2020-08-17 22:03:57 +0200

    SRFI-19: Update leap second table

--- %3a19/srfi-19.scm
+++ %3a19/srfi-19.scm
@@ -194,7 +194,10 @@
 ;; each entry is ( utc seconds since epoch . # seconds to add for tai )
 ;; note they go higher to lower, and end in 1972.
 (define tm:leap-second-table
- '((1230768000 . 34)
+ '((1483228800 . 37)
+  (1435708800 . 36)
+  (1341100800 . 35)
+  (1230768000 . 34)
   (1136073600 . 33)
   (915148800 . 32)
   (867715200 . 31)
