9fe1e2f3a8328d3c670f54301bea0046def8de45
Amirouche <amirouche@hyper.dev>
2020-11-21 10:19:43 +0100

    srfi-158: generators: gtake: avoid make-coroutine-generator

--- %3a158/srfi-158-impl.scm
+++ %3a158/srfi-158-impl.scm
@@ -326,18 +326,17 @@
 
 ;; gtake
 (define gtake
-  (case-lambda ((gen k) (gtake gen k (eof-object)))
-               ((gen k padding)
-                (make-coroutine-generator (lambda (yield)
-                                            (if (> k 0)
-                                                (let loop ((i 0) (v (gen)))
-                                                  (begin (if (eof-object? v) (yield padding) (yield v))
-                                                         (if (< (+ 1 i) k)
-                                                             (loop (+ 1 i) (gen))
-                                                             (eof-object))))
-                                                (eof-object)))))))
-
-
+  (case-lambda
+   ((gen k) (gtake gen k (eof-object)))
+   ((gen k padding)
+    (define (%gtake gen k)
+      (lambda ()
+        (if (zero? k)
+            (eof-object)
+            (begin
+              (set! k (- k 1))
+              (gen)))))
+    (%gtake (gappend gen (lambda () padding)) k))))
 
 ;; gdrop
 (define (gdrop gen k)
